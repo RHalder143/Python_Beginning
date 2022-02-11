a331c5eed0bccc2b91f1828ab34bed952f0da05c
RHalder143 <97688045+RHalder143@users.noreply.github.com>
2022-02-11 12:08:49 +0530

    Add files via upload

---
+++ Additon_2_Numbers.py
@@ -0,0 +1,13 @@
+#Ritam Halder
+#Addition Program
+print("Enter Two Numbers= ")
+
+a=input()
+b=input()
+
+a=float(a)
+b=float(b)
+
+c=a+b
+
+print(c)
---
+++ Print_Assignment.py
@@ -0,0 +1,4 @@
+print("Ritam Halder")
+print("12021002026046")
+print("B.TECH 1st Year")
+print("CSE(AIML)")
